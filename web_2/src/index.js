ecbf809dee96ac9eba15efc98d8a5da24c75b25f
허재웅 <koolerjaebee@gmail.com>
2020-01-29 17:22:32 +0900

    done mytemplate of login&register, WIP in redux&API connect

--- web_2/src/index.js
+++ web_2/src/index.js
@@ -5,12 +5,18 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './modules';
+import createSagaMiddleWare from 'redux-saga';
+import rootReducer, { rootSaga } from './modules';
 
+const sagaMiddleware = createSagaMiddleWare();
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(sagaMiddleware)),
+    );
 
-const store = createStore(rootReducer, composeWithDevTools());
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
     <Provider store={store}>
---
+++ web_2/src/lib/api/auth.js
@@ -0,0 +1,12 @@
+import client from './client';
+
+// 로그인
+export const login = ({ username, password }) =>
+  client.post('/#', { username, password });
+
+// 회원가입
+export const register = ({ username, password }) =>
+  client.post('/#', { username, password }); // 이메일, 이름, 위치
+
+// 로그인 상태 확인
+export const check = () => client.get('/#');
